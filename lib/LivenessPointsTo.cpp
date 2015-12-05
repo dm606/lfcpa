eb118bfd17f9fce9bfbade7ed8edf0735d58a3b8
Dylan McDermott <dylanmcdermott9@gmail.com>
2015-12-05 16:22:53 +0000

    Add liveness information

--- lib/LivenessPointsTo.cpp
+++ lib/LivenessPointsTo.cpp
@@ -7,25 +7,26 @@
 #include "../include/LivenessPointsTo.h"
 
 void LivenessPointsTo::runOnFunction(Function &F) {
+    // Points-to information
     DenseMap<const Instruction *, SmallVector<std::pair<PointsToNode*, PointsToNode*>, 10> *> ain, aout;
+    // Liveness information
+    DenseMap<const Instruction *, SmallVector<PointsToNode*, 10> *> lin, lout;
 
     // Create vectors to store the points-to information in.
     for (auto &BB : F) {
         for (auto &I : BB) {
             ain.insert(std::make_pair(&I, new SmallVector<std::pair<PointsToNode*, PointsToNode*>, 10>()));
             aout.insert(std::make_pair(&I, new SmallVector<std::pair<PointsToNode*, PointsToNode*>, 10>()));
+            lin.insert(std::make_pair(&I, new SmallVector<PointsToNode*, 10>()));
+            lout.insert(std::make_pair(&I, new SmallVector<PointsToNode*, 10>()));
         }
     }
 
     for (auto &BB : F) {
-        Value *last = NULL;
         for (auto &I : BB) {
-            auto instruction_in = ain.find(&I)->second, instruction_out = aout.find(&I)->second;
-            if (last != NULL) {
-                std::pair<PointsToNode*, PointsToNode*> p = std::make_pair(factory.getNode(&I), factory.getNode(last));
-                instruction_in->push_back(p);
-            }
-            last = &I;
+            auto instruction_ain = ain.find(&I)->second, instruction_aout = aout.find(&I)->second;
+            auto instruction_lin = lin.find(&I)->second, instruction_lout = lout.find(&I)->second;
+
         }
     }
 
